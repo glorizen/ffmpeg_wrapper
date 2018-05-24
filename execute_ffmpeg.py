daab6dd35a374048107135a04dd6d11410704ec6
glorizen <dev.trace.off@gmail.com>
2018-05-24 18:44:26 +0500

    ordered method definitions.

--- execute_ffmpeg.py
+++ execute_ffmpeg.py
@@ -482,40 +482,6 @@ def handle_subtitle_extraction(params):
     print('\n'); exit(0)
 
 ##################################################################################################
-def process_encoding_settings(params):
-
-  if not params.get('crf'):
-    
-    if params.get('rs') and len(params['rs']) == 2:
-      resulting_height = int(params['rs'][1])
-    else:
-      try:
-        resulting_height = params['dim'][1]
-      except:
-        resulting_height = 720
-
-    if resulting_height > 900 and resulting_height <= 1100:
-      crf = 23
-    elif resulting_height > 700 and resulting_height <= 900:
-      crf = 21
-    elif resulting_height > 500 and resulting_height <= 700:
-      crf = 19
-    elif resulting_height > 300 and resulting_height <= 500:
-      crf = 18
-
-    if params.get('hevc'):
-      crf -= 2
-
-    params['crf'] = crf
-
-  if not params.get('aqm'):
-    params['aqm'] = 3
-  if not params.get('aqs'):
-    params['aqs'] = 1.00 if not params.get('hevc') else 0.8
-
-  return params
-
-##################################################################################################
 def handle_subtitle_trimming(params, subtitle_filename):
 
   if not len(times_list) >= 1:
@@ -589,6 +555,40 @@ def handle_subtitle_trimming(params, subtitle_filename):
   new_subs.save(subtitle_filename)
   print('Trimmed file written to: [%s]' % (subtitle_filename))
   print('#' * 50)
+
+##################################################################################################
+def process_encoding_settings(params):
+
+  if not params.get('crf'):
+    
+    if params.get('rs') and len(params['rs']) == 2:
+      resulting_height = int(params['rs'][1])
+    else:
+      try:
+        resulting_height = params['dim'][1]
+      except:
+        resulting_height = 720
+
+    if resulting_height > 900 and resulting_height <= 1100:
+      crf = 23
+    elif resulting_height > 700 and resulting_height <= 900:
+      crf = 21
+    elif resulting_height > 500 and resulting_height <= 700:
+      crf = 19
+    elif resulting_height > 300 and resulting_height <= 500:
+      crf = 18
+
+    if params.get('hevc'):
+      crf -= 2
+
+    params['crf'] = crf
+
+  if not params.get('aqm'):
+    params['aqm'] = 3
+  if not params.get('aqs'):
+    params['aqs'] = 1.00 if not params.get('hevc') else 0.8
+
+  return params
 
 ##################################################################################################
 def handle_muxing(params, options):
