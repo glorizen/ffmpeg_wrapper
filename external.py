db0866de23d0a9910ff31f1f85014f95c998fc1f
glorizen <dev.trace.off@gmail.com>
2018-05-25 23:57:10 +0500

    refactored external execution to catch errors from logs.

--- external.py
+++ external.py
@@ -10,42 +10,46 @@ def start_external_execution(external_command, catchphrase=None):
     external_command = external_command.replace('  ', ' ')
 
   temp_name = 'temp_%s' % (str(time.time()).replace('.', ''))
-  dumpfile_name = 'dump_%s' % (str(time.time()).replace('.', ''))
 
   if catchphrase:
-    external_command = external_command + ' &> %s' % (dumpfile_name)
+    tempfile = open(temp_name, 'a')
+  else:
+    tempfile = open(temp_name, 'w')
+     
+  print('_' * 50 + '\n' + '_' * 50 + '\n')
+  print('Starting external job...\n[%s]' % (external_command))
+  print('_' * 50 + '\n' + '_' * 50 + '\n')
+  
+  if catchphrase:
+    process = subprocess.Popen(external_command, shell=True,
+      stdout=tempfile, stderr=tempfile)
+    print('Dumping data [%s] to catch errors, if any.' % (temp_name))
+    process.wait()
 
-  with open(temp_name, 'w') as f:
-    print('_' * 50 + '\n' + '_' * 50 + '\n')
-    print('Starting external job...\n[%s]' % (external_command))
-    print('_' * 50 + '\n' + '_' * 50 + '\n')
-    process = subprocess.Popen(external_command, shell=True, stdout=subprocess.PIPE)
-    
-    if catchphrase:
-      print('Dumping data [%s] to catch errors, if any.' % (dumpfile_name))
+  else:
+    process = subprocess.Popen(external_command, shell=True,
+      stdout=subprocess.PIPE)
 
     for line in iter(process.stdout.readline, b''):
       line = line.decode('utf8')
 
       try:
         sys.stdout.write(line)
-        f.write(line)
+        tempfile.write(line)
       except:
         pass
 
+    try:
+      os.remove(temp_name)
+    except PermissionError:
+      pass
+
   print('_' * 50 + '\n' + '_' * 50 + '\n')
 
-  try:
-    os.remove(temp_name)
-  except PermissionError:
-    pass
-  
-  process.wait()
   if catchphrase:
-    tee_content = open(dumpfile_name, 'r').readlines()
-
+    tee_content = open(temp_name, 'r').readlines()
     try:
-      os.remove(dumpfile_name)
+      os.remove(temp_name)
     except PermissionError:
       pass
 
